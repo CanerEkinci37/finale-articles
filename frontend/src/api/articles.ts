4a8712c663365803bc6357cbb850f2d96d537867
Caner Ekinci <canereknc37@gmail.com>
2024-12-31 12:30:22 +0300

    added getAll function

--- frontend/src/api/articles.ts
+++ frontend/src/api/articles.ts
@@ -1,29 +1,9 @@
 import api from '../utils'
-import { Article, CreateArticleDto, UpdateArticleDto } from '../types/Article';
-
+import { ArticleRead} from '../types/Article';
 
 export const articlesApi = {
-  getAll: async (): Promise<Article[]> => {
-    const response = await api.get<Article[]>('/articles');
+  getAll: async (): Promise<ArticleRead[]> => {
+    const response = await api.get<ArticleRead[]>('/articles');
     return response.data;
-  },
-
-  getById: async (id: number): Promise<Article> => {
-    const response = await api.get<Article>(`/articles/${id}`);
-    return response.data;
-  },
-
-  create: async (data: CreateArticleDto): Promise<Article> => {
-    const response = await api.post<Article>('/articles', data);
-    return response.data;
-  },
-
-  update: async (id: number, data: UpdateArticleDto): Promise<Article> => {
-    const response = await api.put<Article>(`/articles/${id}`, data);
-    return response.data;
-  },
-
-  delete: async (id: number): Promise<void> => {
-    await api.delete(`/articles/${id}`);
   },
 }; 
