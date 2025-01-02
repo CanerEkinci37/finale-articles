2c453f3a92c62b209c830583290f70e53f5a93b4
Caner Ekinci <canereknc37@gmail.com>
2025-01-02 09:10:19 +0300

    Added timestamp to ArticleRead from types

--- frontend/src/types/Article.ts
+++ frontend/src/types/Article.ts
@@ -1,10 +1,20 @@
 import { UserRead } from "./User";
 
+export interface ArticleCreate {
+  title: string;
+  content: string;
+}
+
 export interface ArticleRead {
   id: string;
   title: string;
   content: string;
   author: UserRead;
-  created_at: Date;
-  updated_at: Date;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface ArticleUpdate {
+  title: string;
+  content: string
 }
